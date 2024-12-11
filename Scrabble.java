953a1a5e184e3cc13743381ab58697747bf6ff3f
shachaf-steinberg <Shachaf.steinberg@post.runi.ac.il>
2024-12-11 20:53:41 +0200

    Completed Scrabble.java

--- Scrabble.java
+++ Scrabble.java
@@ -1,6 +1,9 @@
 /*
  * RUNI version of the Scrabble game.
  */
+
+import java.util.logging.Handler;
+
 public class Scrabble {
 
 	// Note 1: "Class variables", like the five class-level variables declared below,
@@ -48,24 +51,42 @@ public class Scrabble {
 
 	// Checks if the given word is in the dictionary.
 	public static boolean isWordInDictionary(String word) {
-		//// Replace the following statement with your code
-		return false;
+	boolean result = false;	
+	for (int i = 0; i < DICTIONARY.length; i++) {
+		if (DICTIONARY[i] == word) {
+		result = true;
+		}
+	}
+	return result;
 	}
 	
 	// Returns the Scrabble score of the given word.
 	// If the length of the word equals the length of the hand, adds 50 points to the score.
 	// If the word includes the sequence "runi", adds 1000 points to the game.
 	public static int wordScore(String word) {
-		//// Replace the following statement with your code
-		return 0;
+		int score = 0;
+		for (int i = 0; i < word.length(); i++) {
+		int letterValue = SCRABBLE_LETTER_VALUES[word.charAt(i) - 'a'];
+		score += letterValue;
+		}
+		score = score * word.length();
+		if (word.length() == HAND_SIZE) {
+			score += 50;
+		}
+		if (MyString.subsetOf("runi", word)) {
+			score += 1000;
+		}	
+		return score;
 	}
 
 	// Creates a random hand of length (HAND_SIZE - 2) and then inserts
 	// into it, at random indexes, the letters 'a' and 'e'
 	// (these two vowels make it easier for the user to construct words)
 	public static String createHand() {
-		//// Replace the following statement with your code
-		return null;
+		String letters = MyString.randomStringOfLetters((HAND_SIZE - 2));
+		letters = MyString.insertRandomly('e', letters);
+		letters = MyString.insertRandomly('a', letters);
+		return letters;
 	}
 	
     // Runs a single hand in a Scrabble game. Each time the user enters a valid word:
@@ -87,7 +108,18 @@ public class Scrabble {
 			String input = in.readString();
 			//// Replace the following break statement with code
 			//// that completes the hand playing loop
-			break;
+			if (input.equals(".")) {
+				break;
+			}
+			if (!isWordInDictionary(input) && (MyString.subsetOf(input, hand))) {
+				System.out.println("No such word in the dictionary. Try again.");
+			}
+			if (MyString.subsetOf(input, hand)) {
+				System.out.println(input + "earned " + wordScore(input) + " points. Score: " + wordScore(input) + " points\n");
+				hand += MyString.remove(input, hand);
+			} else if (!MyString.subsetOf(input, hand)) {
+				System.out.println("Invalid word. Try again.");
+			}
 		}
 		if (hand.length() == 0) {
 	        System.out.println("Ran out of letters. Total score: " + score + " points");
